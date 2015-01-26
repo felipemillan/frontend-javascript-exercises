54ed457b06f8a24959c32b9cf6aff93da28b051c
Felipe Millan <felipe@lanuu.co>
2015-01-26 12:01:09 +0700

    This is my new commit for the exercise, hope it’s correct.

--- 02-reading-mocha-tests/00-a-tested-function/challenge.js
+++ 02-reading-mocha-tests/00-a-tested-function/challenge.js
@@ -2,4 +2,4 @@ describe("linkTo", function() {
   it("should return a valid link for Bloc '<a href='http://www.bloc.io'>Bloc</a>'", function() {
     expect(hello("Bloc", "http://www.bloc.io")).to.equal("<a href='http://www.bloc.io'>Bloc</a>");
   });
-});
+}); 
